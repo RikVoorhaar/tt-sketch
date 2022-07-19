d29f18e9d5dfdc64bf7793cbfce56f57b0678445
Rik Voorhaar <Rik.Voorhaar@unige.ch>
2022-07-19 12:44:40 +0200

    cookie plot

--- scripts/plot_cookie.py
+++ scripts/plot_cookie.py
@@ -1,146 +1,69 @@
 # %%
-import logging
-from itertools import product
-from time import perf_counter
-from typing import Tuple
-
+import pandas as pd
 import numpy as np
-from tqdm import tqdm
-from tt_sketch.tensor import TensorSum, TensorTrain
-from tt_sketch.tt_gmres import ROUNDING_MODE, round_tt_sum, tt_sum_gmres
+import matplotlib.pyplot as plt
 
-from cookie_problem import prepare_cookie_problem
-from experiment_base import Experiment
-
-LOG_FILE = "cookie.log"
-logging.basicConfig(
-    filename=LOG_FILE,
-    level=logging.INFO,
-    format="%(asctime)s: %(message)s",
-)
-
-map_sum, B, precond_map = prepare_cookie_problem(100, 4)
-x0 = TensorTrain.zero(shape=map_sum.in_shape, rank=1)
 
 csv_filename = "results/cookie_2x2.csv"
-experiment = Experiment(csv_filename)
+
+df = pd.read_csv(csv_filename, index_col=0)
+df.drop(columns=df.columns[0:4], inplace=True, axis=1)
+
+for c in ["final_round_ranks"]:
+    df[c] = df[c].apply(eval).apply(np.array)
 
 
-def experiment_cookie(
-    _,
-    rounding_method: ROUNDING_MODE,
-    rounding_method_final: ROUNDING_MODE,
-    max_rank: int,
-    final_round_ranks: Tuple[int, ...],
-    **kwargs,
-):
-    logging.info("\n")
-    logging.info("-" * 80)
-    logging.info(f"Rounding method: {rounding_method}")
-    logging.info(f"Rounding method final: {rounding_method_final}")
-    logging.info(f"Max rank: {max_rank}")
-    logging.info(f"Final round ranks: {final_round_ranks}")
-    logging.info(f"Kwargs: {kwargs}")
-    logging.info("\n")
-    result, history = tt_sum_gmres(
-        A=map_sum,
-        b=B,
-        precond=precond_map,
-        x0=x0,
-        max_rank=max_rank,
-        final_round_rank=None,
-        maxiter=50,
-        tolerance=0,
-        symmetric=True,
-        rounding_method=rounding_method,
-        rounding_method_final=None,
-        verbose=True,
-    )
-    experiment_result = {}
-    experiment_result["last_residual"] = history["residual_norm"][-1]
-    experiment_result["gmres_time"] = np.sum(history["step_time"])
-    # experiment_result["final_round_time"] = history["final_round_time"]
-
-    true_errors = []
-    precond_errors = []
-    round_timings = []
-    for rank in final_round_ranks:
-        current_time = perf_counter()
-        result_rounded = round_tt_sum(
-            result, rank, method=rounding_method_final, eps=None  # type: ignore
-        )
-        round_timings.append(perf_counter() - current_time)
-        true_error = map_sum(result_rounded).error(B, relative=True)
-        precond_error = TensorSum(
-            [precond_map(t) for t in map_sum(result_rounded).tensors]
-        ).error(precond_map(B), relative=True)
-        true_errors.append(true_error)
-        precond_errors.append(precond_error)
-        logging.info(f"Rank {rank}: true error: {true_error:.4e}")
-        logging.info(f"Rank {rank}: preconditioned error: {precond_error:.4e}")
-
-    experiment_result["true_error"] = np.array(true_errors)
-    experiment_result["precond_error"] = np.array(precond_errors)
-    experiment_result["final_round_time"] = np.array(round_timings)
-
-    return experiment_result
-
-
-sketch_ranks = list(range(10, 101, 5))
-pairwise_ranks = list(range(10, 51, 5))
-round_methods = ["sketch", "pairwise"]
-final_round_methods = ["sketch", "pairwise"]
-final_round_ranks = tuple(range(10, 101, 5))
-runs = list(range(20))
-
-
-all_experiments = []
-for run in runs:
-    all_experiments.extend(
-        list(
-            product(["sketch"], ["sketch"], sketch_ranks, [final_round_ranks], [run])
-        )
-    )
-    all_experiments.extend(
-        list(
-            product(
-                ["pairwise"], ["sketch"], pairwise_ranks, [final_round_ranks], [run]
-            )
-        )
-    )
-    all_experiments.extend(
-        list(
-            product(
-                ["sketch"], ["pairwise"], sketch_ranks, [final_round_ranks], [run]
-            )
-        )
-    )
-    all_experiments.extend(
-        list(
-            product(
-                ["pairwise"],
-                ["pairwise"],
-                pairwise_ranks,
-                [final_round_ranks],
-                [run],
-            )
+def process_sl(sl):
+    return np.array(
+        eval(
+            ",".join([s.replace("\n", "") for s in sl])
+            .replace(",,", ",")
+            .replace(",,", ",")
         )
     )
 
-for (
-    rounding_method,
-    rounding_method_final,
-    max_rank,
-    final_round_ranks,
-    run,
-) in tqdm(all_experiments):
-    experiment.do_experiment(
-        None,
-        "cookie_2x2",
-        experiment_cookie,
-        rounding_method=rounding_method,
-        rounding_method_final=rounding_method_final,
-        max_rank=max_rank,
-        final_round_ranks=final_round_ranks,
-        run=run,
-    )
+
+for c in ["true_error", "precond_error", "final_round_time"]:
+    df[c] = df[c].str.split(" ").apply(process_sl)
+
+initial_columns = [
+    "rounding_method",
+    "rounding_method_final",
+    "max_rank",
+    "run",
+    "gmres_time",
+]
+initial_df = df[initial_columns]
+final_columns = [
+    "final_round_ranks",
+    "true_error",
+    "precond_error",
+    "final_round_time",
+]
+final_df = df[final_columns]
+
+row_dfs = []
+for row in final_df.itertuples():
+    row_idx = row[0]
+    row_df = pd.DataFrame(np.stack(row[1:], axis=1), columns=final_columns)
+    row_df["row_idx"] = row_idx
+    row_dfs.append(row_df)
+
+row_dfs = pd.concat(row_dfs)
+initial_df.index.name = "row_idx"
+plot_df = initial_df.merge(row_dfs, on="row_idx")
+plot_df["total_time"] = plot_df["gmres_time"] + plot_df["final_round_time"]
+# %%
+for rounding_method in ["sketch", "pairwise"]:
+    plot_df_rm = plot_df[plot_df["rounding_method"] == rounding_method]
+    plot_df.groupby(["rounding_method", "max_rank"])["true_error"].min()
+    plot_df_rm = plot_df_rm.sort_values("total_time")
+    cummin = plot_df_rm["true_error"].cummin()
+    plt.plot(plot_df_rm["total_time"], cummin, label=rounding_method)
+
+plt.yscale("log")
+plt.xscale("log")
+plt.title("Relative error vs. time for rounding methods used in body of TT-GMRES")
+plt.legend()
+plt.ylabel("Relative error")
+plt.xlabel("Total time")
