9e8f7c9ed0bc780a167507d2b2e6084b0ec91a44
Rik Voorhaar <Rik.Voorhaar@unige.ch>
2022-07-25 15:36:14 +0200

    sbatch in profile

--- scripts/profile.sh
+++ scripts/profile.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
+#SBATCH --job-name=tt-sketch-timings
+#SBATCH --partition=private-math-cpu
+#SBATCH --time=00:30:00
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=64
+#SBATCH --mem-per-cpu=4000 # in MB
+#SBATCH --mail-user=rik.voorhaar@unige.ch
+#SBATCH --mail-type=ALL
 python -m cProfile -o hmt-$HOSTNAME.pstats simple_timing.py --method="HMT"
 python -m cProfile -o stta-$HOSTNAME.pstats simple_timing.py --method="STTA"
