eed811e98db35d3fee15a1d9a95d8833ec3cf1ee
Rik Voorhaar <rik.voorhaar@gmail.com>
2022-07-26 16:32:12 +0200

    timings computed

--- scripts/timings.sh
+++ scripts/timings.sh
@@ -3,7 +3,7 @@
 #SBATCH --partition=private-math-cpu
 #SBATCH --time=12:00:00
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=64
+#SBATCH --cpus-per-task=128
 #SBATCH --mem-per-cpu=4000 # in MB
 #SBATCH --mail-user=rik.voorhaar@unige.ch
 #SBATCH --mail-type=ALL
